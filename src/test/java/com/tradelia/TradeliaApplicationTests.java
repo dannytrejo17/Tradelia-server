af37b87cd17be176092ee69155ed61a507f32cba
dannytrejo17 <trejodanny187@gmail.com>
2026-03-10 12:00:00 +0100

    setup: estructura base del proyecto Spring Boot

---
+++ src/test/java/com/tradelia/TradeliaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tradelia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeliaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
